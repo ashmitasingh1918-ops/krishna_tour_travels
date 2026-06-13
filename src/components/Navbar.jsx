b08f6df2fc0adb6d4af0543ff1fc605e8c95e11f
ashmita-singh1918 <ashmitasingh1918@gmail.com>
2026-06-13 23:29:51 +0530

    Update navbar logo and branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,53 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "../assets/logo.png";
+
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
+
       <div className="container-fluid">
 
-        <Link className="navbar-brand" href="/">
-          Krishna Tours
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          
+          <img
+            src={logo}
+            alt="Krishna Tours Logo"
+            className="logo-img"
+          />
+
+          <span className="brand-text">
+            KRISHNA TOURS
+          </span>
+
         </Link>
 
-    <ul className="navbar-nav mx-auto gap-4">
-        
+        <ul className="navbar-nav ms-auto gap-4">
+
           <li className="nav-item">
-  <Link className="nav-link" to="/">
-    Home
-  </Link>
-</li>
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
 
-<li className="nav-item">
-  <Link className="nav-link" to="/about">
-    About
-  </Link>
-</li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">About</Link>
+          </li>
 
-<li className="nav-item">
-  <Link className="nav-link" to="/fleet">
-    Our Fleet
-  </Link>
-</li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/fleet">Our Fleet</Link>
+          </li>
 
-<li className="nav-item">
-  <Link className="nav-link" to="/packages">
-    Tour Packages
-  </Link>
-</li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/packages">Tour Packages</Link>
+          </li>
 
-<li className="nav-item">
-  <Link className="nav-link" to="/contact">
-    Contact Us
-  </Link>
-</li>
-   </ul>       
-          
-        
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">Contact Us</Link>
+          </li>
+
+        </ul>
 
       </div>
+
     </nav>
   );
 }
